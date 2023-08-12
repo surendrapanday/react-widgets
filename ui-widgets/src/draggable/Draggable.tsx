385e21a7f13b84a4d35c598d09926ea0e6aff212
surendrapanday <panday.sp@gmail.com>
2023-08-12 22:38:27 +1000

    added dragging events

--- ui-widgets/src/draggable/Draggable.tsx
+++ ui-widgets/src/draggable/Draggable.tsx
@@ -1,18 +1,71 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import "./Draggable.css";
-import { useDragEvent } from "../hooks/EventHooks";
+import { useDraggableElement } from "../hooks/EventHooks";
 
 const Draggable = () => {
   const ref = useRef<HTMLDivElement>(null);
-  
-const position = useDragEvent(ref)
+  const [active, setActive] = useState(false)
+
+
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+
+  const start = (event: any) => {
+    const box = ref.current;
+    if (box) {
+      event = event || window.event;
+      event.preventDefault();
+      pos3 = event.offsetX;
+      pos4 = event.offsetY;
+      box.onmouseup = stop;
+      box.onmousemove = drag;
+    }
+  };
+
+  const stop = () => {
+    const boundingBox = ref.current;
+    if (boundingBox) {
+      boundingBox.onmouseup = null;
+      boundingBox.onmousemove = null;
+    }
+  };
+
+  const drag = (event: any) => {
+    const boundingBox = ref.current;
+
+    const bondaryBox = document.getElementById('wrapper')
+
+    const {
+      left = 0,
+      right = 0,
+      top = 0,
+      bottom = 0,
+    } = boundingBox?.getBoundingClientRect() || {};
+
+    if (boundingBox) {
+      event = event || window.event;
+      if (
+        event.clientX > left &&
+        event.clientX < right &&
+        event.clientY > top &&
+        event.clientY < bottom
+      ) {
+        event.preventDefault();
+        pos1 = pos3 - event.clientX;
+        pos2 = pos4 - event.clientY;
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+        boundingBox.style.top = boundingBox.offsetTop - pos2 + "px";
+        boundingBox.style.left = boundingBox.offsetLeft - pos1 + "px";
+      }
+    }
+  };
 
   return (
-    <div
-      className="draggable"
-      ref={ref}
-      style={{ top: position.top, left: position.left }}
-    >
+    <div className="draggable" ref={ref} onMouseDown={start}>
       <p>Hello World</p>
     </div>
   );
--- ui-widgets/src/hooks/EventHooks.ts
+++ ui-widgets/src/hooks/EventHooks.ts
@@ -19,8 +19,8 @@ export const useDragEvent = (ref: RefObject<HTMLDivElement>) => {
         handleMouseUp(event)
       );
       ref?.current?.removeEventListener("mouseleave", (event) =>
-      handleMouseMove(event)
-    );
+        handleMouseMove(event)
+      );
       ref?.current?.removeEventListener("mousemove", (event) =>
         handleMouseMove(event)
       );
@@ -72,4 +72,69 @@ export const useDragEvent = (ref: RefObject<HTMLDivElement>) => {
   };
 
   return position;
+};
+
+export const useDraggableElement = (ref: RefObject<HTMLDivElement>, active: boolean) => {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+useEffect(() => {
+  if (active) {
+    ref.current?.addEventListener('mousedown', start)
+  }
+})
+
+  const start = (event: MouseEvent) => {
+    const box = ref.current;
+    if (box) {
+      event = event || window.event;
+      event.preventDefault();
+      pos3 = event.offsetX;
+      pos4 = event.offsetY;
+      box.onmouseup = stop;
+      box.onmousemove = drag;
+    }
+  };
+
+  const stop = () => {
+    const boundingBox = ref.current;
+    if (boundingBox) {
+      boundingBox.onmouseup = null;
+      boundingBox.onmousemove = null;
+    }
+  };
+
+  const drag = (event: any) => {
+    const boundingBox = ref.current;
+
+    const bondaryBox = document.getElementById('wrapper')
+
+    const {
+      left = 0,
+      right = 0,
+      top = 0,
+      bottom = 0,
+    } = boundingBox?.getBoundingClientRect() || {};
+
+    if (boundingBox) {
+      event = event || window.event;
+      if (
+        event.clientX > left &&
+        event.clientX < right &&
+        event.clientY > top &&
+        event.clientY < bottom
+      ) {
+        event.preventDefault();
+        pos1 = pos3 - event.clientX;
+        pos2 = pos4 - event.clientY;
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+        boundingBox.style.top = boundingBox.offsetTop - pos2 + "px";
+        boundingBox.style.left = boundingBox.offsetLeft - pos1 + "px";
+      }
+    }
+  };
+
 };
