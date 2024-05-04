91e1c97f2230d125c40556b0293f7d3648759e9e
surendrapanday <panday.sp@gmail.com>
2024-05-05 00:43:18 +1000

    draggable

--- ui-widgets/src/draggable/Draggable.tsx
+++ ui-widgets/src/draggable/Draggable.tsx
@@ -1,72 +1,75 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Draggable.css";
-import { useDraggableElement } from "../hooks/EventHooks";
 
 const Draggable = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [active, setActive] = useState(false)
+  const [mousedown, setMouseDown] = useState(false);
+  const [text, setText] = useState('Drag me');
 
+  let pos1: number, pos2: number, pos3: number, pos4: number;
+  pos1 = pos2 = pos3 = pos4 = 0;
 
-  let pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-
-
-  const start = (event: any) => {
+  const start = (event : any) => {
     const box = ref.current;
     if (box) {
-      event = event || window.event;
       event.preventDefault();
       pos3 = event.offsetX;
       pos4 = event.offsetY;
-      box.onmouseup = stop;
-      box.onmousemove = drag;
-    }
-  };
-
-  const stop = () => {
-    const boundingBox = ref.current;
-    if (boundingBox) {
-      boundingBox.onmouseup = null;
-      boundingBox.onmousemove = null;
+      box.addEventListener("mousemove", drag);
+      box.addEventListener("mouseleave", drag);
     }
   };
 
   const drag = (event: any) => {
-    const boundingBox = ref.current;
-
-    const bondaryBox = document.getElementById('wrapper')
-
+    const box = ref.current;
     const {
       left = 0,
       right = 0,
       top = 0,
       bottom = 0,
-    } = boundingBox?.getBoundingClientRect() || {};
+    } = box?.getBoundingClientRect() || {};
 
-    if (boundingBox) {
-      event = event || window.event;
+    if (box) {
       if (
         event.clientX > left &&
         event.clientX < right &&
         event.clientY > top &&
         event.clientY < bottom
       ) {
-        event.preventDefault();
         pos1 = pos3 - event.clientX;
         pos2 = pos4 - event.clientY;
         pos3 = event.clientX;
         pos4 = event.clientY;
-        boundingBox.style.top = boundingBox.offsetTop - pos2 + "px";
-        boundingBox.style.left = boundingBox.offsetLeft - pos1 + "px";
+        box.style.top = box.offsetTop - pos2 + "px";
+        box.style.left = box.offsetLeft - pos1 + "px";
+        setText('Dragging');
       }
+      box.addEventListener("mouseup", stop);
+      box.addEventListener("mouseleave", stop);
     }
   };
 
+  const stop = () => {
+    setText('Drag me')
+    ref.current?.removeEventListener('mousedown', start);
+    ref.current?.removeEventListener('mousemove', drag);
+    ref.current?.removeEventListener('mouseleave', drag);
+  }
+
+  useEffect(() => {
+    ref.current?.addEventListener('mousedown', start)
+    ref.current?.addEventListener('mouseup', stop)
+    ref.current?.addEventListener('mouseleave', stop)
+    return () => {
+      ref.current?.addEventListener('mousedown', start)
+      ref.current?.addEventListener('mouseup', stop)
+      ref.current?.addEventListener('mouseleave', stop)
+    }
+  }, [mousedown, pos1, pos2, pos3, pos4])
+
   return (
     <div className="draggable" ref={ref} onMouseDown={start}>
-      <p>Hello World</p>
+      <p>{text}</p>
     </div>
   );
 };
